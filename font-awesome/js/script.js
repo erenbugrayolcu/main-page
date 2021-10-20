80be8cadbfac6afddb7d34ace84ef94398fe9fea
Eren Bugra YOLCU <yolcubugra7@gmail.com>
2021-10-20 14:59:07 +0300

    Add files via upload

--- font-awesome/js/script.js
+++ font-awesome/js/script.js
@@ -1,5 +1,6 @@
 const dark = document.querySelector('#closed-light');
 const light = document.querySelector('#opened-light');
+let text = document.querySelector('.text');
 let background = document.querySelector('.container');
 
 let state = 'on' 
@@ -9,13 +10,14 @@ function switchLights(){
         light.style.display = 'none';
         dark.style.display = 'block';
         background.style.background = '#fff'
+        text.style.color ='#000';
     }else{
         state = 'off'
         dark.style.display = 'none';
         light.style.display = 'block';
         light.style.color = '#fff'
         background.style.background = '#222'
-
+        text.style.color ='#fff';
     }
 
 }
